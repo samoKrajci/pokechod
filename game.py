9197b451686b3e7786cbce8aa131e3443077005e
samoKrajci <samo.krajci@gmail.com>
2019-04-10 10:10:00 +0200

    push

--- game.py
+++ game.py
@@ -3,15 +3,45 @@ from easygame import *
 open_window('Easy Game!', 800, 600)
 should_quit = False
 
+key = {}
+key['UP'] = False
+key['DOWN'] = False
+key['LEFT'] = False
+key['RIGHT'] = False
+
+
+def move(vect, vel):
+    if['UP']:
+        vect[1] += vel
+    if['DOWN']:
+        vect[1] -= vel
+    if['LEFT']:
+        vect[0] -= vel
+    if['RIGHT']:
+        vect[0] += vel
+    return vect
+
+
+def tlacidka():
+    if type(event) is KeyDownEvent:
+        key[event.key] = True
+    if type(event) is KeyUpEvent:
+        key[event.key] = False
+
+
+vect = [100, 100]
 
 while not should_quit:
     for event in poll_events():
         if type(event) is CloseEvent:
             should_quit = True
+        tlacidka()
+
+    vect = move(vect, 10)
 
     fill(1, 1, 0)
-    draw_circle((400, 300), 300, color=(0.4, 0.3, 0.2, 1))
-    move_camera(position=(10, 10), rotation=None, zoom=None)
+    draw_circle((vect[0], vect[1]), 100, color=(0.4, 0.3, 0.2, 1))
+    move_camera(position=(0, 0), rotation=None, zoom=None)
     next_frame()
 
 close_window()
