149f6724b6592fbe90324892408469a4dfeaf53a
samoKrajci <samo.krajci@gmail.com>
2019-04-10 13:53:12 +0200

    hypermerge

--- game.py
+++ game.py
@@ -2,10 +2,17 @@ from easygame import *
 from math import *
 from random import *
 
+
+def sec(a):
+    return a*40
+
+
 window_height = 1000
 window_width = 1000
 map_width = 1000
 map_height = 1000
+mob_cooldown = sec(5)
+
 open_window('Easy Game!', window_width, window_height)
 should_quit = False
 cam_pos = [0, 0]
@@ -30,29 +37,36 @@ def hud():
         cam_pos[0]-window_width/2+10, cam_pos[1]-window_height/2+10), color=(0, 0, 0, 1))
 
 
+def separate(a, b):
+    dif = fix_rectangle_overlap(a.hitbox, b.hitbox)
+    a.x += dif[0]
+    a.y += dif[1]
+
+
 class Mob:
     def __init__(self, x, y, vel, size):
         self.x = x
         self.y = y
-        self.dead = False
+        self.cooldown = 0
         self.size = size
         self.vel = vel
         self.hitbox = (self.x-self.size*sqrt(2)/2, self.y-self.size*sqrt(2)/2,
                        self.x+self.size*sqrt(2)/2, self.y+self.size*sqrt(2)/2)
 
     def chase(self, target):
+        if self.cooldown != 0:
+            self.cooldown -= 1
+            return
         x_dif = target.x-self.x
         y_dif = target.y-self.y
         x_dif_new = x_dif/sqrt(x_dif**2+y_dif**2+1)*self.vel
         y_dif_new = y_dif/sqrt(x_dif**2+y_dif**2+1)*self.vel
         self.x += x_dif_new
         self.y += y_dif_new
-
-    def attack(self, player):
-        vect = fix_rectangle_overlap(self.hitbox, player.hitbox)
-        print(vect)
+        vect = fix_rectangle_overlap(self.hitbox, target.hitbox)
         if vect != (0, 0):
-            player.hp -= 1
+            target.hp -= 1
+            self.cooldown = mob_cooldown
 
     def update(self):
         draw_circle(center=(self.x, self.y),
@@ -88,7 +102,7 @@ class Player:
             self.x += self.vel
 
 
-dick = Player(30, 3, 10)
+dick = Player(30, 5, 10)
 spawnery = []
 zombiky = []
 
@@ -101,6 +115,11 @@ while not should_quit:
                 spawnery.append([randint(-map_width/2, map_width/2),
                                  randint(-map_height/2, map_height/2)])
         tlacidka()
+
+    for i in zombiky:
+        for j in zombiky:
+            if i != j:
+                separate(i, j)
 
     fill(1, 1, 0)
 
